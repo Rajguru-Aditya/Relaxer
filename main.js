888ca0f61eb9aa633bdfd708551f30873ff69395
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-06-22 17:24:25 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".container");
+const text = document.querySelector("#text");
+
+const totalTime = 7500; // 7.5 seconds (7500)
+const breatheTime = (totalTime / 5) * 2; // 3 seconds (3000)
+const holdTime = totalTime / 5; // 1.5 seconds (1500)
+
+breatheAnimation();
+
+// The animation that changes the text in the circle and change circle size
+function breatheAnimation() {
+  text.innerHTML = "Breathe In!"; // Display breathe in text
+  container.className = "container grow"; // Make the circle expand
+
+  setTimeout(() => {
+    text.innerText = "Hold"; // Display hold text after timer ends (breatheTime = 3 seconds)
+
+    setTimeout(() => {
+      text.innerText = "Breathe Out!"; // Display Breathe out text after timer ends (holdTime = 1.5 seconds)
+      container.className = "container shrink"; // Make the circle contract
+    }, holdTime);
+  }, breatheTime);
+}
+
+// Loop the animation after total time = 7.5 seconds
+setInterval(breatheAnimation, totalTime);
